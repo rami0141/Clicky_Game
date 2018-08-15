b54e0f79625c17a309077da6ee932033a5aab120
Cristina Zhang <rami0141@umn.edu>
2018-08-15 15:26:38 -0500

    components

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,10 @@
+import React from "react";
+import "./Footer.css";
+
+const Footer = () => (
+  <footer className="bg-info text-white text-center">
+    <p>&copy;2018 Cristina Zhang</p>
+  </footer>
+);
+
+export default Footer;
--- src/components/Game/Game.js
+++ src/components/Game/Game.js
@@ -1,6 +1,8 @@
 import React from "react";
 import Nav from "../Nav";
 import Header from "../Header";
+import Footer from "../Footer";
+import Container from "../Container";
 
 export default class Game extends React.Component {
   render() {
@@ -8,6 +10,9 @@ export default class Game extends React.Component {
       <div>
         <Nav />
         <Header />
+        <Container>
+        </Container>
+        <Footer />
       </div>
     );
   }
